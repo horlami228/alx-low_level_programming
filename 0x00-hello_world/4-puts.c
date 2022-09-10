412dd05a472f2b19880614b8d0bc0ab4f515adf6
horlami228 <akintolaolamilekan51@gmail.com>
2022-09-10 04:03:04 +0100

    c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main -printing
+ *
+ * The return is 0 (succesful)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
