d81605585774c29c9ecb831030097159d939d66f
horlami228 <akintolaolamilekan51@gmail.com>
2022-09-10 15:06:06 +0100

    asa

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- size of byte
+ *
+ * Return: always 0 successful
+ *
+*/
+
+int main(void)
+
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of a int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+
+	return (0);
+}
