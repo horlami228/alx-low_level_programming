22f78190d5996c8a195b78fd22910237addd714e
horlami228 <akintolaolamilekan51@gmail.com>
2022-09-10 14:29:42 +0100

    asa

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- print grammer
+ *
+ * Return: always 0 (succesful)
+ *
+*/
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
