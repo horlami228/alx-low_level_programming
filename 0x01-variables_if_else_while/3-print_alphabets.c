1cd3d1661c11eb70c10cf10f57ed5b4d844d0bda
horlami228 <akintolaolamilekan51@gmail.com>
2022-09-12 23:25:23 +0100

    jll

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main -prints all alphabet
+ *
+ *Return: return 0 succesful
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+	putchar(ch);
+
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
+}
+	putchar('\n');
+Return: (0);
+}
+
