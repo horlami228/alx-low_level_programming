b3b78cd389e7810e5e1ccd08ffc1d658e12053e6
horlami228 <akintolaolamilekan51@gmail.com>
2022-09-14 01:37:57 +0100

    asas

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - function to be able to print abc
+ *
+ * Return: 0 succesful
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
+
