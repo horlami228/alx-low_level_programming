72d778f067f3cabd9d098f9eb121bb6656ea69ef
horlami228 <akintolaolamilekan51@gmail.com>
2022-09-17 01:44:58 +0100

    asa

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers -  prints the numbers, from 0 to 9
+ *Return: this program returns nothing
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+		putchar('\n');
+}
