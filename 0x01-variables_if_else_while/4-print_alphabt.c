26d0454b80b660d2ab82827cf064df8640ba8ba8
horlami228 <akintolaolamilekan51@gmail.com>
2022-09-19 18:08:38 +0100

    asa

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
+/**
+ *main - print the alphabet but not q and e
+ *Return: 0 if success
+ */
 int main(void)
 {
 	char pp;
-	
-	for ( pp = 'a'; pp <= 'z'; pp++)
+
+	for (pp = 'a'; pp <= 'z'; pp++)
 	{
 
-	if ( pp == 'q' || pp == 'e')
+	if (pp == 'q' || pp == 'e')
 	{
 		continue;
-	}	
-	
+	}
+
 		putchar(pp);
 	}
 		putchar(10);
-		
+
 		return (0);
 }
