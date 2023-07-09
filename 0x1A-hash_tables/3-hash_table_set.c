1514194365429e99e59ee1a561dc0581b4dba71b
Olamilekan <akintolaolamilekan51@gmail.com>
2023-07-09 13:29:36 +0100

    updated

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,13 +9,12 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	/* check if key or hash table is empty*/
-	if (key == NULL || ht == NULL)
+	if (key == NULL || ht == NULL || value == NULL)
 	{
 		return (0);
 	}
-	hash_table_t *temp = ht;
 	/*get index value*/
-	unsigned long int key_value = key_index(key, temp->size);
+	unsigned long int key_value = key_index(key, ht->size);
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
 	if (new_node == NULL)
@@ -35,16 +34,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	/*check if bucket is empty*/
-	if (temp->array[key_value] == NULL)
+	if (ht->array[key_value] == NULL)
 	{
-		temp->array[key_value] = new_node;
+		ht->array[key_value] = new_node;
 		return (1);
 	}
 	/*check for collision*/
 	else
 	{
-		new_node->next = temp->array[key_value];
-		temp->array[key_value] = new_node;
+		new_node->next = ht->array[key_value];
+		ht->array[key_value] = new_node;
 		return (1);
 	}
 }
