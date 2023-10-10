70ce323020e3a9fad533417decd1122b1b4cbae2
Olamilekan <akintolaolamilekan51@gmail.com>
2023-10-10 13:49:33 +0100

    search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,63 @@
+#include "search_algos.h"
+
+/**
+ * print_array - print the array
+ * @array: array to be printed
+ * @lb: lower bound
+ * @hb: upper bound
+*/
+void print_array(int *array, int lb, int hb)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = lb; i < hb; i++)
+	{
+		printf("%d", array[i]);
+
+		if (i < hb - 1)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar(10);
+}
+
+/**
+ * binary_search - perform a binary search
+ * @array: array to search through
+ * @size: size of the array
+ * @value: target value
+ * Return: value index on success, -1 otherwise
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t lb = 0;
+	size_t ub = size - 1;
+	size_t mid;
+
+	while (lb <= ub)
+	{
+		print_array(array, lb, ub + 1);
+
+		if (value == array[mid])
+		{
+			return (mid);
+		}
+
+		else if (value > array[mid])
+		{
+			lb = mid + 1;
+		}
+
+		else
+		{
+			ub = mid - 1;
+		}
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - jump search algorithm
+ * @array: arrray to retrieve element
+ * @size: size of the array
+ * @value: value to search for
+ * Return: The index on success, -1 on otherwise
+*/
+int jump_search(int *array, size_t size, int value)
+{
+	int low = 0;
+	int jump = sqrt(size);
+	size_t i;
+
+	for (i = 0; i < size; i += jump)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+		if (value == array[i])
+			return (i);
+		else if (value > array[i])
+			low = i;
+		else if (value < array[i])
+			break;
+	}
+
+	printf("Value found between indexes [%d] and [%ld]\n", low, i);
+
+	for (i = low; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+
+	return (-1);
+}
